6d095a78e3df711f2f3246f3989bbe86e83b2c31
Sumityeshirao27 <99062325+sumityeshirao@users.noreply.github.com>
2022-04-21 15:30:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Word ");
+            String n1 = Console.ReadLine();
+            Console.WriteLine("How Many Times ");
+            int no1 = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= no1; i++)
+            {
+                Console.WriteLine("\n " + n1);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
